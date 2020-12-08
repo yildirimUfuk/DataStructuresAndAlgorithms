232f98fc362acbb787b925d5ab8ac2528e55809d
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2020-12-08 19:03:42 +0300

    - solution of longest common prefix problem and its solutions has been added.

---
+++ Training/LongestCommonPrefix.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+#include<vector>
+using namespace std;
+//Write a function to find the longest common prefix string amongst an array of strings.
+//If there is no common prefix, return an empty string "".
+//Example 1:
+//	Input: ["flower", "flow", "flight"]
+//	Output : "fl"
+//
+//Example 2 :
+//	Input : ["dog", "racecar", "car"]
+//	Output : ""
+//	Explanation : There is no common prefix among the input strings.
+//	Note :
+//	All given inputs are in lowercase letters a - z.
+
+class LongestCommonPrefix
+{
+public:
+	string longestCommonPrefix(vector<string>& strs);
+};
+
