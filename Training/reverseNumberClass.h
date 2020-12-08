44bd78e687475504b8dcd8d133c053f21db7264b
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2020-12-08 20:31:54 +0300

    - solution of reverse number problem has been added to project.

---
+++ Training/reverseNumberClass.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class reverseNumberClass
+{
+public:
+	void reverseNumbeTest(int& number);
+	int reverse(int& number);
+private:
+	int reverseNumber(int& number);
+	int leetCodeCozum(int x);
+};
+
