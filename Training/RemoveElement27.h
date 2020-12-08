f5246d81cd0be69f71ca134c34ac38315959a0e1
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2020-12-08 21:07:01 +0300

    - solution of remove element problem has been added to project without any test.

---
+++ Training/RemoveElement27.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+#include<vector>
+using namespace std;
+class RemoveElement27
+{
+public:
+	/// <summary>
+	/// Runtime: 4 ms, faster than 64.49% of C++ online submissions for Remove Element.
+	//Memory Usage : 9.1 MB, less than 32.61 % of C++ online submissions for Remove Element.
+	/// </summary>
+	/// <param name="nums"></param>
+	/// <param name="val"></param>
+	/// <returns></returns>
+	int removeElement(vector<int>& nums, int val);
+	bool removeElementTest();
+};
+
