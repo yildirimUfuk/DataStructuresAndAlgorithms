5aa886cfbd161da9284291dfb061c620760c729e
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2020-12-08 19:20:35 +0300

    - solution of three sum and its tests has been added to the project.

---
+++ Training/_3Sum.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<vector>
+#include<algorithm>
+#include<iostream>
+#include<unordered_map>
+#include<iterator>
+#include<set>
+class _3Sum
+{
+public:
+	//runtime error!
+	std::vector<std::vector<int>> threeSum(std::vector<int>& nums);
+	std::vector<std::vector<int>> threeSum2(std::vector<int>& nums);
+};
+
