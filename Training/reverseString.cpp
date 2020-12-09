ea35b381d3616d10834a1c1700d5b4bc6911f228
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2020-12-09 21:55:47 +0300

    - there is a problem. It will be fixed later.

--- Training/reverseString.cpp
+++ Training/reverseString.cpp
@@ -5,6 +5,6 @@ void reverseString::reverseString1(vector<char>& s)
 	if (s.size() == 0) return;
 	char c = s.front();
 	s.erase(s.begin());
-	reverseString(s);
+	//reverseString(s);
 	s.push_back(c);
 }
