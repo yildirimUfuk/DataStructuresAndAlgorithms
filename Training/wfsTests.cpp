393b862e3a6918c25224f6a5a270afd7b0a3a8a4
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2021-01-12 05:52:39 +0300

    - LeetCode problem 29 has been solved succesfully.

--- Training/wfsTests.cpp
+++ Training/wfsTests.cpp
@@ -3,6 +3,7 @@
 void wfsTests::test()
 {
 	//std::cout << "# linkedList tests #\n"; listTest.allTests();
-	std::cout << "# BST tests #\n"; bstTest.allTest();
+	//std::cout << "# BST tests #\n"; bstTest.allTest();
+	std::cout << "# divide2integers tests #\n"; d.divideTwoIntegersTests();
 	
 }
