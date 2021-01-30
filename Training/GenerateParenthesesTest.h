bddc67e88683802412da5c137c5243e40f48addf
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2021-01-30 18:34:00 +0300

    - Generate parentheses problems tests had been written.

---
+++ Training/GenerateParenthesesTest.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"GenerateParentheses.h"
+class GenerateParenthesesTest
+{
+private:
+	GenerateParentheses generateParentheses;
+	std::vector<bool>accuracy;
+	bool test1();
+	bool test2();
+public:
+	void globalTests();
+
+};
+
